591d60869588d78d9fd60ff8038265182654e21e
achmad_syah_hidayat <achmad.hidayat@adira.co.id>
2023-12-22 09:36:44 +0700

    tambah fitur edit kontak dan field terisi pada kontak yang ingin diupdate

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -10,6 +10,12 @@ use Illuminate\View\View;
 
 class ContactsController extends Controller{
     private $limit = 3;
+    private $rules=[
+        'name' => ['required', 'min:5'],
+        'company' => ['required'],
+        'email' => ['Required', 'email']
+    ];
+
     public function index(Request $request){
         if ($group_id = ($request->get('group_id'))){
             $contacts = Contact::where('group_id', $group_id)->orderBy('id', 'desc')->paginate($this->limit);
@@ -21,16 +27,20 @@ class ContactsController extends Controller{
         return view('contacts.index', compact('contacts'));
     }
 
+    // untuk tambah baru kontak
     public function create(){
         return view("contacts.create");
     }
 
+    // untuk edit kontak
+    public function edit(contact $contact):View {
+        // $contact = Contact::find($contacts);
+        return view("contacts.edit", compact('contact'));
+    }
+
     public function store(Request $request){
-        $rules=[
-            'name' => ['required', 'min:5'],
-            'company' => ['required'],
-            'email' => ['Required', 'email']
-        ];
+        // dirubah pemanggilan class menjadi lokal, dengan cara dibawah ini
+        $this-> validate($request, $this->rules);
 
         $this-> validate($request, $rules);
 
@@ -41,6 +51,24 @@ class ContactsController extends Controller{
         Contact::create($request->all());
 
         // setelah tersimpan, akan di direct ke halaman index. lalu dibuat message jika berhasil tersimpan
-        return redirect('contacts')->with('message', 'Contact berhasil tersimpan');
+        return redirect('contacts')->with('message', 'Kontak berhasil tersimpan');
+    }
+
+    public function update($id, Request $request){
+
+        // dirubah pemanggilan class menjadi lokal, dengan cara dibawah ini
+        $this-> validate($request, $this->rules);
+
+        // untuk mengambil data kontak sebelumnya, di variable sementara contact
+        $contact = Contact::find($id);
+
+        // code dibawah ini tadinya untuk menandakan jika form submit client berhasil maka message muncul
+        // die('contact berhasil tersimpan');
+
+        // untuk mengupdate data kontak lalu disimpan ke database
+        $contact->update($request->all());
+
+        // setelah diupdate, akan di direct ke halaman index. lalu dibuat message jika berhasil update
+        return redirect('contacts')->with('message', 'Kontak berhasil diperbaharui');
     }
 }
---
+++ resources/views/contacts/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('content')
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <strong>Edit Contact</strong>
+    </div>       
+    {!! Form::model ($contact, ['route' => ['contacts.update', $contact->id], 'method' => 'PATCH']) !!}    
+    
+    @include("contacts.form")
+
+    {!! Form::close() !!}
+  </div>
+@endsection
